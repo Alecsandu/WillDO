a6c7bc6829e329eda2066fe814008e6c90710d2b
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2020-08-29 17:18:37 +0300

    Added more infos

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/Styles"));
-app.use(express.static(__dirname + "/Scripts"));
-app.use(express.static(__dirname + "/Images"));
+app.use(express.static(__dirname + "/styles"));
+app.use(express.static(__dirname + "/scripts"));
+app.use(express.static(__dirname + "/images"));
 
 app.get("/", function (req, res) {
   res.render("index");
